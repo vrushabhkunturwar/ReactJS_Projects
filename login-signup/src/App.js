3303c665ef9a6bb1ef775478ac4fda2a131f7eda
vrushabhkunturwar <vrushabhkunturwar@gmail.com>
2024-08-23 17:53:09 +0530

    Sign up And Login Form Created

---
+++ login-signup/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import LoginSignUp from './Components/LoginSignUp/LoginSignUp';
+
+
+
+function App() {
+  return (
+    <div >
+      <LoginSignUp/>
+    </div>
+  );
+}
+
+export default App;
